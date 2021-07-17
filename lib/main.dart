f659469d55a30d71077f2632c4519f6cfdb5dad7
Mameng57 <takiya2888157@gmail.com>
2021-07-17 17:08:52 +0700

    fixes branch problems, moved to master

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
 
---
+++ lib/screens/categories.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+      ),
+      children: [
+        
+      ],
+    );
+  }
+}
