81cf846a4872a2610beff1ace51986942b43e37c
Mameng57 <takiya2888157@gmail.com>
2022-04-04 18:29:16 +0700

    added global theme settings

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:digital_recipebook/home.dart';
+import 'package:digital_recipebook/category_meals_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Digital Recipebook",
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.orange[200],
+          shadowColor: Colors.transparent,
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            fontFamily: "RobotoCondensed",
+          ),
+        ),
+        primaryColor: Colors.orange[200],
+        textTheme: TextTheme(
+          headline5: TextStyle(
+            fontSize: 20,
+            color: Colors.black54,
+            fontWeight: FontWeight.w600,
+            fontFamily: "Raleway",
+          ),
+          headline6: TextStyle(
+            color: Colors.white,
+            fontFamily: "Raleway",
+            fontWeight: FontWeight.bold,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.black45,
+            fontFamily: "Raleway",
+            fontWeight: FontWeight.normal,
+          )
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => Home(),
+        CategoryMealsScreen.routeName: (_) => CategoryMealsScreen(),
+      },
+    );
+  }
+}
