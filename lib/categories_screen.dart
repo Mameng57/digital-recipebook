1f8be72905ca5560bcda028d096a860021e9d23f
Mameng57 <takiya2888157@gmail.com>
2022-04-04 18:30:10 +0700

    refactor screenpage for categories

---
+++ lib/categories_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:digital_recipebook/DUMMY_DATA.dart';
+import 'package:digital_recipebook/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        mainAxisSpacing: 15,
+        crossAxisSpacing: 10,
+      ),
+      padding: EdgeInsets.all(10),
+      children: DUMMY_CATEGORIES
+          .map(
+            (category) => CategoryItem(
+              category.id,
+              category.title,
+              category.color,
+            ),
+          )
+          .toList(),
+    );
+  }
+}
